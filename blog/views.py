492f8d11c799abfeb214e693c2b46454f0e775c6
Fatemeh Hajimazdarani <hajimazdarani.f@gmail.com>
2023-04-17 19:17:38 +0430

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -6,3 +6,8 @@ def post_list(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
